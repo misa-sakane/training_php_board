67452a43f22792ff727409f97022cd7bbe6bd962
misa-sakane <misa.sakane@seattleconsulting.co.jp>
2022-05-12 12:27:27 +0900

    ログイン画面作成

--- creatAccount.php
+++ creatAccount.php
@@ -2,7 +2,7 @@
 
 <head>
     <link rel="stylesheet" href="login.css">
-    <title>ログイン|Bullentin board</title>
+    <title>新規追加|Bullentin board</title>
 </head>
 
 <body>
@@ -13,28 +13,26 @@
 
     <div class="main">
         <h1>Bullentin board</h1>
-        <p>ログイン画面</p>
+        <p>新規追加画面</p>
     </div>
 
     <div class="authorization">
      <form method="post" action="login">
         <div class="authorization-form">
-            <h2>ログイン</h2>
+            <h2>新規追加</h2>
             <p>ユーザーIDとパスワードを入力してください。</p>
         </div>
 
         <div class="forms">
             <input type="text" autocomplete="off" placeholder="ユーザーID">
             <input type="text" autocomplete="off" placeholder="パスワード">
+            <input type="text" autocomplete="off" placeholder="パスワード確認">
         </div>
 
         <div class="login-button">
-            <input onclick="location.href='login'" value="ログインする">
+            <input onclick="location.href='login'" value="登録する">
         </div>
 
-        <div class="newAccount-button">
-          <a href="creatAccount.php">新規追加はこちら</a>
-         </div>
       </from>
     </div>
 

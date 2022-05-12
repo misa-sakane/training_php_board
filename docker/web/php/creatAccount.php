0303b239622c203faa0a46e4c5733a98c01ad504
misa-sakane <misa.sakane@seattleconsulting.co.jp>
2022-05-12 17:19:58 +0900

    新規追加画面作成

--- docker/web/php/creatAccount.php
+++ docker/web/php/creatAccount.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/creatAccount.css">
     <title>新規追加|Bullentin board</title>
 </head>
 
@@ -10,8 +10,11 @@
         <div class="header-logo">Bullentin board</div>
     </div>
     
+    <div class="return-botton">
+        <input onclick="location.href='../../../index.php'" value="戻る">
+    </div>
 
-    <div class="main">
+    <div class="mainArea">
         <h1>Bullentin board</h1>
         <p>新規追加画面</p>
     </div>
@@ -25,8 +28,10 @@
 
         <div class="forms">
             <input type="text" autocomplete="off" placeholder="ユーザーID">
+          <div class="passward-form">
             <input type="text" autocomplete="off" placeholder="パスワード">
             <input type="text" autocomplete="off" placeholder="パスワード確認">
+          </div> 
         </div>
 
         <div class="login-button">

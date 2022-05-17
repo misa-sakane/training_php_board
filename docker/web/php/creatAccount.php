ac963cfba9483d2074b6d7e9894d58cb0f2b0894
misa-sakane <misa.sakane@seattleconsulting.co.jp>
2022-05-17 15:26:22 +0900

    Revert "タスク5を削除したため"
    This reverts commit 0c0641e87929577ebd37e52a8e1eabdfbb6103fe.

--- docker/web/php/creatAccount.php
+++ docker/web/php/creatAccount.php
@@ -14,30 +14,33 @@
         <input onclick="location.href='../../../index.php'" value="戻る">
     </div>
 
-    <div class="main-area">
+    <div class="mainArea">
         <h1>Bullentin board</h1>
         <p>新規追加画面</p>
     </div>
 
     <div class="authorization">
-        <form method="post" action="">
+        <form method="post" action="login">
             <div class="authorization-form">
                 <h2>新規追加</h2>
                 <p>ユーザーIDとパスワードを入力してください。</p>
             </div>
 
             <div class="forms">
-                <input type="text" name="userId" maxlength=20 autocomplete="off" placeholder="ユーザーID">
+                <input type="text" autocomplete="off" placeholder="ユーザーID">
                 <div class="passward-form">
-                    <input type="password" name="password" maxlength=30 autocomplete="off" placeholder="パスワード">
-                    <input type="password" name="passwordCheck" maxlength=30 autocomplete="off" placeholder="パスワード確認">
+                    <input type="text" autocomplete="off" placeholder="パスワード">
+                    <input type="text" autocomplete="off" placeholder="パスワード確認">
                 </div>
             </div>
+
             <div class="login-button">
-                <input type="submit" name="signUp" value="登録する">
+                <input onclick="location.href='login'" value="登録する">
             </div>
+
             </from>
     </div>
+
 </body>
 
 </html>

0c0641e87929577ebd37e52a8e1eabdfbb6103fe
misa-sakane <misa.sakane@seattleconsulting.co.jp>
2022-05-17 15:18:13 +0900

    タスク5を削除したため

--- docker/web/php/creatAccount.php
+++ docker/web/php/creatAccount.php
@@ -14,33 +14,30 @@
         <input onclick="location.href='../../../index.php'" value="戻る">
     </div>
 
-    <div class="mainArea">
+    <div class="main-area">
         <h1>Bullentin board</h1>
         <p>新規追加画面</p>
     </div>
 
     <div class="authorization">
-        <form method="post" action="login">
+        <form method="post" action="">
             <div class="authorization-form">
                 <h2>新規追加</h2>
                 <p>ユーザーIDとパスワードを入力してください。</p>
             </div>
 
             <div class="forms">
-                <input type="text" autocomplete="off" placeholder="ユーザーID">
+                <input type="text" name="userId" maxlength=20 autocomplete="off" placeholder="ユーザーID">
                 <div class="passward-form">
-                    <input type="text" autocomplete="off" placeholder="パスワード">
-                    <input type="text" autocomplete="off" placeholder="パスワード確認">
+                    <input type="password" name="password" maxlength=30 autocomplete="off" placeholder="パスワード">
+                    <input type="password" name="passwordCheck" maxlength=30 autocomplete="off" placeholder="パスワード確認">
                 </div>
             </div>
-
             <div class="login-button">
-                <input onclick="location.href='login'" value="登録する">
+                <input type="submit" name="signUp" value="登録する">
             </div>
-
             </from>
     </div>
-
 </body>
 
 </html>
---
+++ docker/web/php/posts.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" href="../css/posts.css">
+    <title>投稿一覧|Bullentin board</title>
+</head>
+
+<body>
+    <div class="header-left">
+        <div class="header-logo">Bullentin board</div>
+        <div id="humMenu">
+            <input id="humCheck" type="checkbox">
+            <div>
+            </div>
+
+
+            <div class="mainArea">
+                <h1>投稿一覧</h1>
+
+</body>
+
+</html>

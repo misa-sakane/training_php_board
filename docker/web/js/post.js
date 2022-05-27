7fbfc3b348c004408356251243aa5d966f8b682f
misa-sakane <misa.sakane@seattleconsulting.co.jp>
2022-05-27 09:37:54 +0900

    タスク7のコメント修正

--- docker/web/js/post.js
+++ docker/web/js/post.js
@@ -7,23 +7,23 @@ $(function() {
      * 
      * @return void
      */
-    function postValidaton(inputTitle,inputContent){
+    function postValidaton(inputTitle, inputContent) {
         let errors = [];
         //必須項目のチェック
-        if(inputTitle =="" || inputContent ==""){
+        if (inputTitle == "" || inputContent == "") {
             errors.push("項目が未入力です。\n");
         }
         //投稿タイトルの文字数制限チェック
-        if(inputTitle.length>20){
+        if (inputTitle.length > 20) {
             errors.push("投稿タイトルを20文字以下で入力してください。\n");
         }
         //投稿内容の文字数制限チェック
-        if(inputContent.length>200){
+        if (inputContent.length > 200) {
             errors.push("投稿タイトルを200文字以下で入力してください。\n");
         }
         //エラーが１つでもヒットしていたらエラー文表示
-        if (errors.length>0) {
-            let errormessage = errors.join(""); 
+        if (errors.length > 0) {
+            let errormessage = errors.join("");
             return errormessage;
         }
     }
@@ -32,31 +32,31 @@ $(function() {
     btnSubmit.addEventListener('click', function(event) {
         const inputTitle = document.getElementById('form-title').value;
         const inputContent = document.getElementById('form-content').value;
-        const errors = postValidaton(inputTitle,inputContent);
+        const errors = postValidaton(inputTitle, inputContent);
         if (errors) {
             alert(errors);
             return;
         }
         $.ajax({
-            type:'POST',
-            url:'../php/ajax.php',
-            datatype:'json',
-            data:{
-                'class':'postsTable',
-                'func':'addPostData',
-                'postTitle': inputTitle,
-                'postContent': inputContent,
-            },
+                type: 'POST',
+                url: '../php/ajax.php',
+                datatype: 'json',
+                data: {
+                    'class': 'postsTable',
+                    'func': 'addPostData',
+                    'postTitle': inputTitle,
+                    'postContent': inputContent,
+                },
             })
-        .done(function(data){
+            .done(function(data) {
                 $('#post-modal').fadeOut();
                 $('.sp-nav').fadeOut();
                 $('#js-black-bg').fadeOut();
                 getAddPostDataBase();
-            }) 
-        .fail(function (data){
-            alert('通信失敗');
-        })
+            })
+            .fail(function(data) {
+                alert('通信失敗');
+            })
     })
 
     /**
@@ -64,23 +64,24 @@ $(function() {
      * 
      * @return void
      */
-    function getPostDataBase(){
+    function getPostDataBase() {
         $.ajax({
-            type:'POST',
-            url:'../php/ajax.php',
-            datatype:'json',
-            data:{
-                'class':'postsTable',
-                'func':'getPostData',
-            },
+                type: 'POST',
+                url: '../php/ajax.php',
+                datatype: 'json',
+                data: {
+                    'class': 'postsTable',
+                    'func': 'getPostData',
+                },
             })
-        .done(function(data){
-            $.each(data,function(key,value){
-                $('#post-data').append('<tr><td>'+'<input type="checkbox"></td><td>'+value.seq_no + '</td><td>' +value.user_id + '</td><td>' + value.post_date + '</td><td>'+ value.post_title +'<br>'+value.post_contents+'</td><td><i class="fa-solid fa-pen-to-square"></i></td><td>&times;</td></tr>'
-        )});
-        }).fail(function (data){
-            alert('通信失敗');
-        })
+            .done(function(data) {
+                $.each(data, function(key, value) {
+                    $('#post-data').append('<tr><td>' + '<input type="checkbox"></td><td>' + value.seq_no + '</td><td>' + value.user_id + '</td><td>' + value.post_date + '</td><td>' + value.post_title + '<br>' + value.post_contents + '</td><td><i class="fa-solid fa-pen-to-square"></i></td><td>&times;</td></tr>')
+                });
+            })
+            .fail(function(data) {
+                alert('通信失敗');
+            })
     }
 
     /**
@@ -88,23 +89,24 @@ $(function() {
      * 
      * @return void
      */
-    function getAddPostDataBase(){
+    function getAddPostDataBase() {
         $.ajax({
-            type:'POST',
-            url:'../php/ajax.php',
-            datatype:'json',
-            data:{
-                'class':'postsTable',
-                'func':'getnewPostData',
-            },
+                type: 'POST',
+                url: '../php/ajax.php',
+                datatype: 'json',
+                data: {
+                    'class': 'postsTable',
+                    'func': 'getnewPostData',
+                },
             })
-        .done(function(data){
-            $.each(data,function(key,value){
-                $('#post-data').append('<tr><td>'+'<input type="checkbox"></td><td>'+value.seq_no + '</td><td>' +value.user_id + '</td><td>' + value.post_date + '</td><td>'+ value.post_title +'<br>'+value.post_contents+'</td><td><i class="fa-solid fa-pen-to-square"></i></td><td>&times;</td></tr>'
-        )});
-        }).fail(function (data){
-            alert('通信失敗');
-        })
+            .done(function(data) {
+                $.each(data, function(key, value) {
+                    $('#post-data').append('<tr><td>' + '<input type="checkbox"></td><td>' + value.seq_no + '</td><td>' + value.user_id + '</td><td>' + value.post_date + '</td><td>' + value.post_title + '<br>' + value.post_contents + '</td><td><i class="fa-solid fa-pen-to-square"></i></td><td>&times;</td></tr>')
+                });
+            })
+            .fail(function(data) {
+                alert('通信失敗');
+            })
     }
 
     //getPostDataBase関数の呼び出す
@@ -114,18 +116,18 @@ $(function() {
     const nav = document.getElementById('nav-wrapper');
     const hamburger = document.getElementById('js-hamburger');
     const blackBg = document.getElementById('js-black-bg');
-    hamburger.addEventListener('click', function () {
+    hamburger.addEventListener('click', function() {
         nav.classList.toggle('open');
     });
-    blackBg.addEventListener('click', function () {
+    blackBg.addEventListener('click', function() {
         nav.classList.remove('open');
     });
 
     //投稿追加を押した時の処理
-    $('#add-post').click(function(){
+    $('#add-post').click(function() {
         $('#post-modal').fadeIn();
     });
-    $('#close-modal').click(function(){
+    $('#close-modal').click(function() {
         $('#post-modal').fadeOut();
-    });  
+    });
 });
--- docker/web/php/Validation.php
+++ docker/web/php/Validation.php
@@ -66,7 +66,6 @@ class Validation
             if (password_verify($loginpassword,  $selectuserinfo['password'])) {
                 //DBのユーザー情報をセッションに保存
                 $_SESSION['loginId'] =  $selectuserinfo['user_id'];
-                error_log($_SESSION['loginId']);
             } else {
                 $errors = $errors . "ユーザーIDもしくはパスワードが間違っています。" . '\n';
             }
